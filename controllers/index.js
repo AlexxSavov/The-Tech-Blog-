8ae572c361ee9272565ed14f6ce1463f43b583ed
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-08-21 19:28:15 -0400

    code fixup mixup

--- controllers/index.js
+++ controllers/index.js
@@ -9,4 +9,8 @@ router.use('/dashboard', dashboardRoutes);
 
 router.use('/api', apiRoutes);
 
+router.use((req, res) => {
+    res.status(404).end();
+  }); 
+
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 // import all models
 const Post = require('./Post');
-const User = require('./User');
-const Vote = require('./Vote');
-const Comment = require('./Comment');
+const Comment = require('./Comments');
 
 
 //Associations
